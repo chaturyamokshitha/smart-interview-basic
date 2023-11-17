e4c9febbe07e334de6ea3064378961d144e16e30
chaturya mokshitha <127294370+chaturyamokshitha@users.noreply.github.com>
2023-11-17 20:55:37 +0530

    Computeapowerb.js

---
+++ Computeapowerb.js
@@ -0,0 +1,42 @@
+Given two integers a and b, compute a power b.
+Note: Do not use any inbuilt functions/libraries for your main logic.
+
+Input Format
+
+First and only line of input contains two positive integers a and b.
+
+Constraints
+
+1 <= a <= 100
+0 <= b <= 9
+
+Output Format
+
+Print a power b.
+
+Sample Input 0
+
+2 3
+Sample Output 0
+
+8
+Explanation 0
+
+Self Explanatory.
+
+
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+      
+        System.out.print((long)Math.pow(a,b));
+    }
+}
